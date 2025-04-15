b1cee227e89c827e2f94b8bdfad015056c7ea347
subtosolo <207770976+subtosolo@users.noreply.github.com>
2025-04-15 22:54:43 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ README.md
@@ -0,0 +1,31 @@
+# sizzzlent
+
+quickly shorten your long ass links
+
+
+### prerequisites
+
+
+1. download the code
+2. run this command
+
+```bash
+npm i
+```
+3. to use it, run this
+
+```bash
+npm start
+```
+## deploy
+
+this repo is made in nodejs, which means you can only host this on a few platforms. i personally recommend [render](https://render.com/deploy?repo=https://github.com/szvy/sizzlent)
+
+
+## credits
+
+* [szvy](https://github.com/szvy) - only developer lowkey
+* [tinyurl](https://tinyurl.com) - inspiration
+
+## license
+this website is registered under the [MIT license](LICENSE)
---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const app = express();
+const urlDatabase = {};
+
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'frontend')));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
+});
+
+app.post("/api/shorten", (req, res) => {
+  const { alias, longUrl, domain } = req.body;
+  if (!longUrl) {
+    return res.status(400).json({ error: "URL is required" });
+  }
+
+  const validDomains = ["sizzlent.com", "szl.my"];
+  if (!validDomains.includes(domain)) {
+    return res.status(400).json({ error: "invalid domain selected" });
+  }
+
+  if (urlDatabase[alias]) {
+    return res.status(400).json({ error: "alias already taken, please choose another one" });
+  }
+  const shortAlias = alias || Math.random().toString(36).substr(2, 6);
+  urlDatabase[shortAlias] = longUrl;
+  const shortUrl = `http://${domain}/${shortAlias}`;
+  res.json({ shortUrl });
+});
+
+
+app.get("/:alias", (req, res) => {
+  const alias = req.params.alias;
+  const longUrl = urlDatabase[alias];
+  if (longUrl) {
+    res.redirect(longUrl);
+  } else {
+    res.status(404).send("ts was NOT found 😭🙏");
+  }
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`ts is running on http://localhost:${PORT}`);
+});
